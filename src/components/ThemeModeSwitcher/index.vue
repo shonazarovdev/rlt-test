77c9ec13d0c4d8c58407340d74aae331f67c0911
Muhammad <mukhammaddev9@gmail.com>
2025-01-31 22:21:29 +0500

    theme switch logic

--- src/components/ThemeModeSwitcher/index.vue
+++ src/components/ThemeModeSwitcher/index.vue
@@ -2,11 +2,27 @@
 import { computed } from "vue";
 
 const themeLabel = computed(() => localStorage.getItem("color-theme"));
+
+const handleThemeSwitch = (e) => {
+  const isDark = e.target.checked;
+  localStorage.setItem("color-theme", isDark);
+
+  if (isDark) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+};
 </script>
 
 <template>
   <label class="inline-flex items-center cursor-pointer">
-    <input type="checkbox" value="" class="sr-only peer" />
+    <input
+      type="checkbox"
+      value=""
+      class="sr-only peer"
+      @change="handleThemeSwitch"
+    />
     <div
       class="relative w-11 h-6 dark:bg-dark_primary peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"
     ></div>
