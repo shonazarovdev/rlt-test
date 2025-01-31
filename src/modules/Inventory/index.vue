b7bb24b8d78f0489380844c70aba91016f0ef3e0
Muhammad <mukhammaddev9@gmail.com>
2025-01-31 18:22:56 +0500

    inventory border changes

--- src/modules/Inventory/index.vue
+++ src/modules/Inventory/index.vue
@@ -4,12 +4,12 @@ const list = Array(25);
 
 <template>
   <div
-    class="h-full rounded-xl overflow-hidden border-[0.5px] dark:border-dark_border grid grid-cols-5 grid-rows-5"
+    class="h-full dark:bg-dark_secondary rounded-xl overflow-hidden border-2 dark:border-dark_border grid grid-cols-5 grid-rows-5"
   >
     <div
       v-for="item in list"
       :key="item"
-      class="border-[0.5px] dark:border-dark_border"
+      class="border dark:border-dark_border -m-[1px]"
     ></div>
   </div>
 </template>
