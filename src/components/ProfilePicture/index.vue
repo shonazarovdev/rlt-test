5dffe97121ec35b287665a3b1b15cb95ab0d925a
Muhammad <mukhammaddev9@gmail.com>
2025-01-31 18:18:50 +0500

    changed profile picture height

--- src/components/ProfilePicture/index.vue
+++ src/components/ProfilePicture/index.vue
@@ -3,7 +3,7 @@
 <template>
   <picture>
     <img
-      class="w-full h-full max-h-[35%] object-cover rounded-lg"
+      class="w-full h-full max-h-[40%] object-cover rounded-lg"
       src="/images/blur.png"
       alt="Profile_picture"
     />
