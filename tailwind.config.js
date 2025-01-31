99aa38521f460255c9fe4b5d57ceb4ab5b189234
Muhammad <mukhammaddev9@gmail.com>
2025-01-31 16:56:42 +0500

    theme configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: "#f00",
+        light_primary: "#fefefe",
+        dark_primary: "#1D1D1D",
       },
     },
   },
